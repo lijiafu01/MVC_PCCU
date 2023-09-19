8240c8d618a9d3c1ae94e55489c583deddf48e15
Enzo2601 <nguyenviethoan3333@gmail.com>
2023-09-19 16:59:29 +0800

    update

--- MVC_PCCU/MVC_PCCU/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_PCCU.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
